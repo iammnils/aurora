eaa4c2cb7f7985451fe3c1a22a87a9e0c4cf2dce
Ammersken Nils <nilsammersken@gmail.com>
2022-06-25 02:02:57 +0200

    feat: add tailwind and daisyui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<h1 class="text-7xl tracking-tight font-bold">I Love DaisyUI</h1>
+
+<button class="btn rounded-none">Button</button>
+<button class="btn btn-primary rounded-none">Smash that Button</button>
+<button class="btn btn-secondary rounded-none">Button</button>
+<button class="btn btn-accent rounded-none">Button</button>
+<button class="btn btn-ghost rounded-none">Button</button>
+<button class="btn btn-link rounded-none">Button</button>
+
+<div class="stack">
+	<div class="text-center border border-base-content card w-36 rounded-none bg-base-100">
+		<div class="card-body">A</div>
+	</div>
+	<div class="text-center border border-base-content card w-36 rounded-none bg-base-100">
+		<div class="card-body">B</div>
+	</div>
+	<div class="text-center border border-base-content card w-36 rounded-none bg-base-100">
+		<div class="card-body">C</div>
+	</div>
+	<div class="text-center border border-base-content card w-36 rounded-none bg-base-100">
+		<div class="card-body">C</div>
+	</div>
+	<div class="text-center border border-base-content card w-36 rounded-none bg-base-100">
+		<div class="card-body">C</div>
+	</div>
+	<div class="text-center border border-base-content card w-36 rounded-none bg-base-100">
+		<div class="card-body">C</div>
+	</div>
+</div>
