6878924284283164c70c9822c7f6c182cb473972
Ammersken Nils <nilsammersken@gmail.com>
2022-06-28 01:42:22 +0200

    feat: build rudimental card system

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import TrashIcon from '$lib/assets/icons/TrashIcon.svelte';
+
+	let flipped = false;
+	export let question = 'What does aurora mean?';
+	export let solution =
+		'A luminous phenomenon that consists of streamers or arches of light appearing in the upper atmosphere of a planet`s magnetic polar regions.';
+
+	function flipCard(): void {
+		flipped = !flipped;
+	}
+</script>
+
+<div class="card bg-base-100 border my-6">
+	<div class="card-body flex gap-5">
+		{#if !flipped}
+			<h2 class="card-title">Question</h2>
+			<p class="font-mono">{question}</p>
+			<div class="card-actions justify-start">
+				<button on:click={flipCard} class="btn btn-outline">Check Solution</button>
+				<button on:click={flipCard} class="btn btn-ghost ml-auto">
+					<TrashIcon />
+				</button>
+			</div>
+		{:else}
+			<h2 class="card-title">Solution</h2>
+			<p class="font-mono">{solution}</p>
+			<div class="card-actions justify-start">
+				<button on:click={flipCard} class="btn btn-outline btn-warning w-full sm:w-32"
+					>Difficult
+				</button>
+				<button on:click={flipCard} class="btn btn-outline w-full sm:w-32">Just Fine</button>
+				<button on:click={flipCard} class="btn btn-outline btn-success w-full sm:w-32">Easy</button>
+				<button on:click={flipCard} class="btn btn-ghost sm:ml-auto">
+					<TrashIcon />
+				</button>
+			</div>
+		{/if}
+	</div>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,8 @@
 	import '../app.css';
 </script>
 
-<main>
-	<slot />
+<main class="grid place-items-center">
+	<div class="flex flex-col content-between w-full lg:w-2/4 md:w-3/4 sm:w-full px-5 py-5 h-screen">
+		<slot />
+	</div>
 </main>
